b8b3f53ac0efd25ebf3fcdbb844bb26c855d08d8
AshkanAhmadi <ashkan16ma96@gmail.com>
2022-04-10 16:08:05 +0430

    getting ready project

---
+++ src/Container/ContactListApp.js
@@ -0,0 +1,15 @@
+import _ from "lodash";
+
+const ContactListApp = () => {
+  return (
+    <div className="container">
+      <h2>Contact List App</h2>
+      <div>
+        <p>add contact</p>
+        <p>contact list</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactListApp;
